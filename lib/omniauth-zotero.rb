00b763b6f59df0408dadcb38d93b7415d92c5dd0
Jason Morrison <jason.p.morrison@gmail.com>
2012-08-05 18:45:15 +0200

    reorganize lib/

--- lib/omniauth-zotero.rb
+++ lib/omniauth-zotero.rb
@@ -1,7 +1,2 @@
-require "omniauth-zotero/version"
-
-module Omniauth
-  module Zotero
-    # Your code goes here...
-  end
-end
+require "omniauth/zotero/version"
+require "omniauth/strategies/zotero"
---
+++ lib/omniauth/strategies/zotero.rb
@@ -0,0 +1,8 @@
+require 'omniauth-oauth'
+
+module OmniAuth
+  module Strategies
+    class Zotero < OmniAuth::Strategies::OAuth
+    end
+  end
+end
